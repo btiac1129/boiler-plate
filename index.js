3a82cbda66fe07fd196a68c48825fd0053ce7ef1
btiac1129 <btiac1129@gmail.com>
2021-08-01 15:32:13 +0900

    로그인 라우팅  만들기

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 const { User } = require("./models/User");
 
@@ -32,6 +33,38 @@ app.post("/api/users/register", (req, res) => {
             success: true,
         });
     }); // mongodb method
+});
+
+app.post("/login", (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다.",
+            });
+        }
+
+        // 요청한 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인한다.
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다.",
+                });
+            }
+            // 비밀번호까지 맞다면 유저를 위한 Token을 생성해야 한다.
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // 토큰을 쿠키, 로컬스토리지에 저장한다.
+                res
+                    .cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            });
+        });
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -51,6 +52,26 @@ userSchema.pre("save", function(next) {
         next();
     }
 });
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    // plainPassword 1234567  ===  암호화된 비밀번호 ?
+    // plainPassword를 암호화 해서, DB에 있는 암호화 같은지 확인해야 한다. 복호화 할 수는 없어서.
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    // jsonwebtoken 이용해서 토큰을 생성하기
+    var token = jwt.sign(user._id.toHexString(), "secretToken");
+    user.token = token;
+    user.save(function(err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = { User };
