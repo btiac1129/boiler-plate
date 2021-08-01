3b24d4a4c7bc87b32f690870864e673c9cd7d2d8
btiac1129 <btiac1129@gmail.com>
2021-08-01 16:02:45 +0900

    Auth 기능 만들기

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 4000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const auth = require("./middleware/auth");
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -35,7 +36,7 @@ app.post("/api/users/register", (req, res) => {
     }); // mongodb method
 });
 
-app.post("/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
     // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -64,6 +65,21 @@ app.post("/login", (req, res) => {
                     .json({ loginSuccess: true, userId: user._id });
             });
         });
+    });
+});
+
+app.get("api/users/auth", auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해왔다는 얘기는
+    // Authentication이 True 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image,
     });
 });
 
